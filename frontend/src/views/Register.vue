f0941e3ea519021b27aab88d2b829d84cbbdfeb0
doctoey <doctoey@odds.team>
2026-08-26 21:19:37 +0700

    feat: add register form structure

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -2,5 +2,18 @@
 </script>
 
 <template>
-form
+  <main>
+    <h1>Welfare Registration System</h1>
+
+    <form @submit.prevent>
+      <label>
+        National ID
+        <input type="text" />
+      </label>
+
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  </main>
 </template>
