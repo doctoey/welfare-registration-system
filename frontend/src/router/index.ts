9097c0edad1e0bdee09e4c883f5328272622f037
doctoey <doctoey@odds.team>
2026-08-26 21:17:13 +0700

    feat: implement registration view and update router configuration

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Register from "../views/Register.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/", redirect: "/register" }],
+  routes: [
+    { path: "/", redirect: "/register" },
+    { path: "/register", component: Register },
+  ],
 });
 
 export default router;
